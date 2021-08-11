e57c5fe32d3a2abd4c0305708266fc8870ad92ff
Linh <d.linh1196@gmail.com>
2021-08-11 17:26:28 +0700

    Layout for quiz question

---
+++ app/src/main/java/com/example/a08112021_quizapp/Question.kt
@@ -0,0 +1,12 @@
+package com.example.a08112021_quizapp
+
+class Question (
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
---
+++ app/src/main/java/com/example/a08112021_quizapp/QuizQuestionActivity.kt
@@ -0,0 +1,29 @@
+package com.example.a08112021_quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_question.*
+
+class QuizQuestionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Question size", "${questionsList.size}");
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" +  "/" + progressBar.max
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+    }
+}
